bad545f7f293186197ab4d4e6993b6a8c45ae1aa
Doug Hirlinger <doughirlinger@gmail.com>
2024-09-28 11:38:31 -0400

    pt 4 complete

--- src/main/java/org/launchcode/hellospring/HelloController.java
+++ src/main/java/org/launchcode/hellospring/HelloController.java
@@ -2,21 +2,29 @@ package org.launchcode.hellospring;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-
-    @GetMapping("hello")
+    // handles path /hello
+    /*@GetMapping("hello")
     @ResponseBody
     public String hello() {
         return "Hello, Spring!";
-    }
-
+    }*/
+    //handles path /goodbye
     @GetMapping("goodbye")
     @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
+    }
+
+    // handles request of the form / hello?name=LaunchCode
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
     }
 
 }
