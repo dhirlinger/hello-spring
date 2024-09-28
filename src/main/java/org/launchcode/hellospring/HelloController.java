ef5d5df785f72c629d38c87269e1e4dbabd3b33d
Doug Hirlinger <doughirlinger@gmail.com>
2024-09-28 11:51:56 -0400

    pt5 query and path params complete

--- src/main/java/org/launchcode/hellospring/HelloController.java
+++ src/main/java/org/launchcode/hellospring/HelloController.java
@@ -2,6 +2,7 @@ package org.launchcode.hellospring;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,11 +21,18 @@ public class HelloController {
         return "Goodbye, Spring!";
     }
 
-    // handles request of the form / hello?name=LaunchCode
+    // handles request of the form / hello?name=aParam
     @GetMapping("hello")
     @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
+
+    //handles /hello/LaunchCode
+     @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name){
+        return "Hello, " + name + "!";
+     }
 
 }
